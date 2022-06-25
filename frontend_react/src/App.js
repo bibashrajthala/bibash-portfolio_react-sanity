01d3eae91f32f2974b312098761e54fcc8d4fdf6
Bibash Rajthala <bibashrajthala00@gmail.com>
2022-06-25 10:50:06 +0545

    Structure Files and Folders in react

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,9 +1,19 @@
-function App() {
+import React from "react";
+import "./App.scss";
+import { Navbar } from "./components";
+import { Header, About, Skills, Work, Testimonial, Footer } from "./containers";
+const App = () => {
   return (
-    <div className="App">
-      <h1>app</h1>
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
