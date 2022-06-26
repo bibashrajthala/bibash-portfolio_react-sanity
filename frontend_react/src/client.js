c73706a6ca2f479a3a7e3406d8f80f27256197e7
Bibash Rajthala <bibashrajthala00@gmail.com>
2022-06-26 18:31:33 +0545

    connect backend(sanity) with react

---
+++ frontend_react/src/client.js
@@ -0,0 +1,16 @@
+import sanityClient from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+
+export const client = sanityClient({
+  //   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  projectId: "1mg5sgrs",
+
+  dataset: "production",
+  apiVersion: "2022-06-26",
+  useCdn: true,
+  token: process.env.REACT_APP_SANITY_TOKEN,
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
